2174c543b30b74e89184705acdbca326afdc6da9
唐家富 <tangjf@bell.ai>
2021-04-16 10:36:04 +0800

    添加

--- js/事件循环-浏览器.js
+++ js/事件循环-浏览器.js
@@ -1,26 +1,57 @@
-async function async1(){
-    console.log('async1 start')
-    await async2() //
-    console.log('async1 end')
-  }
-async function async2(){
-    console.log('async2')
-}
+// async function async1(){
+//     console.log('async1 start')
+//     await async2() //
+//     console.log('async1 end')
+//   }
+// async function async2(){
+//     console.log('async2')
+// }
+// console.log('script start')
+// setTimeout(function(){
+//     console.log('setTimeout0') 
+// },3)  
+// setTimeout(function(){
+//     console.log('setTimeout3') 
+// },0)  
+// setImmediate(() => console.log('setImmediate'));// node
+// process.nextTick(() => console.log('nextTick'));// node
+// async1();
+// new Promise(function(resolve){
+//     console.log('promise1')
+//     resolve();
+//     console.log('promise2')
+// }).then(function(){
+//     console.log('promise3')
+// })
+// console.log('script end')
+
+
+console.log( '***********打印 第二题***********');
+
 console.log('script start')
-setTimeout(function(){
-    console.log('setTimeout0') 
-},3)  
-setTimeout(function(){
-    console.log('setTimeout3') 
-},0)  
-setImmediate(() => console.log('setImmediate'));// node
-process.nextTick(() => console.log('nextTick'));// node
-async1();
-new Promise(function(resolve){
-    console.log('promise1')
-    resolve();
-    console.log('promise2')
-}).then(function(){
-    console.log('promise3')
+
+async function async1() {
+  await async2()
+  console.log('async1 end')
+}
+async function async2() {
+  console.log('async2 end')
+}
+async1()
+
+setTimeout(function() {
+  console.log('setTimeout')
+}, 0)
+
+new Promise(resolve => {
+  console.log('Promise')
+  resolve()
 })
+  .then(function() {
+    console.log('promise1')
+  })
+  .then(function() {
+    console.log('promise2')
+  })
+
 console.log('script end')
