2c65bce73b533992ea2e2b29159a9e0a8d04cefa
LittleTangJF <18681268108@163.com>
2021-08-24 13:53:22 +0800

    cc

--- src/demo.js
+++ src/demo.js
@@ -48,6 +48,7 @@
 
 console.log('*********************打印');
 console.log('**************aa*******打印');
+console.log('**************cc*******打印');
 // function removeRepeat(str) {
 //     let arr = str.split('')
 //     let newArr = Array.from(new Set(arr))
